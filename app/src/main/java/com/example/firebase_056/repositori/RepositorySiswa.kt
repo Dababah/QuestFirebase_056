e2a9e53ed47b3f2cc0370860d9bbd060e6ac0506
Dababah <pawasalee96@gamil.com>
2026-01-02 23:58:38 +0700

    benerin logic get data sama post data ke firestore pake

--- app/src/main/java/com/example/firebase_056/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/firebase_056/repositori/RepositorySiswa.kt
@@ -4,14 +4,7 @@ import com.example.firebase_056.modeldata.Siswa
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
 
-interface RepositorySiswa {
-    suspend fun getDataSiswa(): List<Siswa>
-    suspend fun postDataSiswa(siswa: Siswa)
-}
 
-class FirebaseRepositorySiswa : RepositorySiswa {
-    private val db = FirebaseFirestore.getInstance()
-    private val collection = db.collection("siswa")
 
     override suspend fun getDataSiswa(): List<Siswa> {
         return try {
@@ -28,4 +21,15 @@ class FirebaseRepositorySiswa : RepositorySiswa {
         }
     }
 
-    
+    override suspend fun postDataSiswa(siswa: Siswa) {
+        val docRef = if (siswa.id == 0L) collection.document() else
+            collection.document(siswa.id.toString())
+        val data = hashMapOf(
+            "id" to (siswa.id.takeIf { it != 0L } ?: docRef.id.hashCode()),
+            "nama" to siswa.nama,
+            "alamat" to siswa.alamat,
+            "telpon" to siswa.telpon
+        )
+        docRef.set(data).await()
+    }
+}
