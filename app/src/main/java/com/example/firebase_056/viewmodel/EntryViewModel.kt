3ddb521e0f10f565458c2363bfa6f0938c323803
Dababah <pawasalee96@gamil.com>
2026-01-09 00:34:20 +0700

    implementasi fungsi update data siswa

--- app/src/main/java/com/example/firebase_056/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/firebase_056/viewmodel/EntryViewModel.kt
@@ -8,7 +8,25 @@ import com.example.firebase_056.repositori.RepositorySiswa
 import com.example.firebase_056.modeldata.toDataSiswa
 import androidx.compose.runtime.*
 
-si untuk menyimpan data yang di-entry */
+class EntryViewModel(private val repositorySiswa: RepositorySiswa) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean
+    {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput
+                (detailSiswa))
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
     suspend fun addSiswa() {
         if (validasiInput()) {
             repositorySiswa.postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
