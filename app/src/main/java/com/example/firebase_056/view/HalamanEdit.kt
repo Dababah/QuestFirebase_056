7a258247abd9292e3ef0667d1c7ba0e8f8f53068
Dababah <pawasalee96@gamil.com>
2026-01-09 00:39:13 +0700

    hubungkan viewmodel ke entry body untuk edit data

---
+++ app/src/main/java/com/example/firebase_056/view/HalamanEdit.kt
@@ -0,0 +1,16 @@
+package com.example.firebase_056.view
+{ innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.editSatuSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
