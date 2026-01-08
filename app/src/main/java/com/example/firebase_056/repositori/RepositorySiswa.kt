03927e3f5a7e51e865201b3c149ce41778f344cf
Dababah <pawasalee96@gamil.com>
2026-01-09 01:02:28 +0700

    implementasi editSatuSiswa menggunakan query id di firestore

--- app/src/main/java/com/example/firebase_056/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/firebase_056/repositori/RepositorySiswa.kt
@@ -1,10 +1,15 @@
 
 
-
-    override suspend fun hapusSatuSiswa(id: Long) {
+    override suspend fun editSatuSiswa(id: Long, siswa: Siswa) {
         val docQuery = collection.whereEqualTo("id", id).get().await()
         val docId = docQuery.documents.firstOrNull()?.id ?: return
-        collection.document(docId).delete().await()
+        collection.document(docId).set(
+            mapOf(
+                "id" to siswa.id,
+                "nama" to siswa.nama,
+                "alamat" to siswa.alamat,
+                "telpon" to siswa.telpon
+            )
+        ).await()
     }
-}
 
